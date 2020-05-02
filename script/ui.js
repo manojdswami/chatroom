a0c94baa481c7e432003496eefc641d5d1f98518
Manoj Swami <manojdswami@gmail.com>
2020-05-03 00:53:41 +0530

    full implementation

---
+++ script/ui.js
@@ -0,0 +1,27 @@
+class ChatUI {
+
+    constructor(list) {
+        this.list = list;
+    }
+
+    clear() {
+        this.list.innerHTML = '';
+    }
+
+    render(data) {
+        const when = dateFns.distanceInWordsToNow(
+            data.created_at.toDate(),
+            { addSuffix: true }
+        );
+
+        const html = `
+            <li class='list-group-item'>
+                <span class='name'>${data.username}</span>
+                <span class='message'>${data.message}</span>
+                <div class='time'>${when}</div>
+            </li>
+        `;
+
+        this.list.innerHTML += html;
+    }
+}
